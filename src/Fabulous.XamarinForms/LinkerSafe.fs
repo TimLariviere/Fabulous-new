1c628373cda58a65b65e2f16118180f015193268
Timothé Larivière <timothe.lariviere@outlook.com>
2022-01-25 20:00:39 +0100

    Add assembly attribute linker safe

---
+++ src/Fabulous.XamarinForms/LinkerSafe.fs
@@ -0,0 +1,9 @@
+﻿namespace Fabulous.XamarinForms
+
+type LinkerSafeAttribute () =
+    inherit System.Attribute()
+
+/// Mark Fabulous.XamarinForms as safe for linking
+/// This means users selecting "Link framework SDKs only" will get a fully tree-shaken Fabulous.XamarinForms.dll
+[<assembly:LinkerSafe>]
+do()
---
+++ src/Fabulous/LinkerSafe.fs
@@ -0,0 +1,9 @@
+﻿namespace Fabulous
+
+type LinkerSafeAttribute () =
+    inherit System.Attribute()
+
+/// Mark Fabulous as safe for linking
+/// This means users selecting "Link framework SDKs only" will get a fully tree-shaken Fabulous.dll
+[<assembly:LinkerSafe>]
+do()
