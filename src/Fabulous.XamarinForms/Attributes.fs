382e34caa3dbdd3920e96772038f11be62763893
Timothé Larivière <timothe.lariviere@outlook.com>
2022-01-26 11:39:00 +0100

    Trying to avoid storing BindableProperty

--- src/Fabulous.XamarinForms/Attributes.fs
+++ src/Fabulous.XamarinForms/Attributes.fs
@@ -17,27 +17,32 @@ module Attributes =
                     target.ClearValue(bindableProperty)
                 else
                     target.SetValue(bindableProperty, value))
+                    
+    let bindableUpdateNode<'a> (newValueOpt: 'a voption) (node: IViewNode) (bindableProperty: Xamarin.Forms.BindableProperty) =
+        let target = node.Target :?> BindableObject
+        match newValueOpt with
+        | ValueNone -> target.ClearValue(bindableProperty)
+        | ValueSome v -> target.SetValue(bindableProperty, v)
 
     let inline defineBindableWithComparer<'inputType, 'modelType, 'valueType>
+        key
         (bindableProperty: BindableProperty)
         (convert: 'inputType -> 'modelType)
         (convertValue: 'modelType -> 'valueType)
         (compare: 'modelType -> 'modelType -> bool)
         =
         Attributes.defineCustomScalar<'inputType, 'modelType, 'valueType, BindableProperty>
-            bindableProperty.PropertyName
+            key
             bindableProperty
             convert
             convertValue
             compare
-            (fun newValueOpt node bindableProperty ->
-                let target = node.Target :?> BindableObject
-                match newValueOpt with
-                | ValueNone -> target.ClearValue(bindableProperty)
-                | ValueSome v -> target.SetValue(bindableProperty, v))
+            bindableUpdateNode
+                
+    let equalCompare<'T when 'T : equality> (x: 'T) (y: 'T) = x = y
 
-    let inline defineBindable<'T when 'T: equality> bindableProperty =
-        defineBindableWithComparer<'T, 'T, 'T> bindableProperty id id (=)
+    let inline defineBindable<'T when 'T: equality> key bindableProperty =
+        defineBindableWithComparer<'T, 'T, 'T> key bindableProperty id id equalCompare
 
     let inline defineAppThemeBindable<'T when 'T: equality> (bindableProperty: BindableProperty) =
         Attributes.defineCustomScalar<AppThemeValues<'T>, AppThemeValues<'T>, AppThemeValues<'T>, BindableProperty>
